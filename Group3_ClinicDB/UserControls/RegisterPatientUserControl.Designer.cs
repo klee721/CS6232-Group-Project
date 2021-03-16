f9f39519d5a286b196bea582f8717564eb0031ba
Jordan Barron <jbarron8@my.westga.edu>
2021-03-16 18:38:44 -0400

    Register and Clear buttons added

--- Group3_ClinicDB/UserControls/RegisterPatientUserControl.Designer.cs
+++ Group3_ClinicDB/UserControls/RegisterPatientUserControl.Designer.cs
@@ -29,18 +29,47 @@ namespace Group3_ClinicDB.UserControls
         /// </summary>
         private void InitializeComponent()
         {
+            this.registerButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // registerButton
+            // 
+            this.registerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registerButton.Location = new System.Drawing.Point(723, 516);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(180, 32);
+            this.registerButton.TabIndex = 0;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(932, 516);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(180, 32);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
             // 
             // RegisterPatientUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.registerButton);
+            this.MaximumSize = new System.Drawing.Size(1215, 615);
+            this.MinimumSize = new System.Drawing.Size(1215, 615);
             this.Name = "RegisterPatientUserControl";
-            this.Size = new System.Drawing.Size(1176, 581);
+            this.Size = new System.Drawing.Size(1215, 615);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Button clearButton;
     }
 }
