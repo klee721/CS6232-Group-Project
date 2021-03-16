e78a385f5733b72e023307fc8e0f4a824d64a757
Jordan Barron <jbarron8@my.westga.edu>
2021-03-16 18:51:55 -0400

    All labels added to RegisterPatientUserControl

--- Group3_ClinicDB/UserControls/RegisterPatientUserControl.Designer.cs
+++ Group3_ClinicDB/UserControls/RegisterPatientUserControl.Designer.cs
@@ -31,6 +31,17 @@ namespace Group3_ClinicDB.UserControls
         {
             this.registerButton = new System.Windows.Forms.Button();
             this.clearButton = new System.Windows.Forms.Button();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.dateOfBirthLabel = new System.Windows.Forms.Label();
+            this.genderLabel = new System.Windows.Forms.Label();
+            this.ssnLabel = new System.Windows.Forms.Label();
+            this.address1Label = new System.Windows.Forms.Label();
+            this.address2Label = new System.Windows.Forms.Label();
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.zipCodeLabel = new System.Windows.Forms.Label();
+            this.phoneNumberLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // registerButton
@@ -53,10 +64,131 @@ namespace Group3_ClinicDB.UserControls
             this.clearButton.Text = "Clear";
             this.clearButton.UseVisualStyleBackColor = true;
             // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastNameLabel.Location = new System.Drawing.Point(58, 61);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(84, 18);
+            this.lastNameLabel.TabIndex = 2;
+            this.lastNameLabel.Text = "Last Name:";
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstNameLabel.Location = new System.Drawing.Point(58, 109);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(85, 18);
+            this.firstNameLabel.TabIndex = 3;
+            this.firstNameLabel.Text = "First Name:";
+            // 
+            // dateOfBirthLabel
+            // 
+            this.dateOfBirthLabel.AutoSize = true;
+            this.dateOfBirthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateOfBirthLabel.Location = new System.Drawing.Point(58, 157);
+            this.dateOfBirthLabel.Name = "dateOfBirthLabel";
+            this.dateOfBirthLabel.Size = new System.Drawing.Size(94, 18);
+            this.dateOfBirthLabel.TabIndex = 4;
+            this.dateOfBirthLabel.Text = "Date of Birth:";
+            // 
+            // genderLabel
+            // 
+            this.genderLabel.AutoSize = true;
+            this.genderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genderLabel.Location = new System.Drawing.Point(58, 199);
+            this.genderLabel.Name = "genderLabel";
+            this.genderLabel.Size = new System.Drawing.Size(61, 18);
+            this.genderLabel.TabIndex = 5;
+            this.genderLabel.Text = "Gender:";
+            // 
+            // ssnLabel
+            // 
+            this.ssnLabel.AutoSize = true;
+            this.ssnLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ssnLabel.Location = new System.Drawing.Point(58, 240);
+            this.ssnLabel.Name = "ssnLabel";
+            this.ssnLabel.Size = new System.Drawing.Size(167, 18);
+            this.ssnLabel.TabIndex = 6;
+            this.ssnLabel.Text = "Social Security Number:";
+            // 
+            // address1Label
+            // 
+            this.address1Label.AutoSize = true;
+            this.address1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.address1Label.Location = new System.Drawing.Point(649, 61);
+            this.address1Label.Name = "address1Label";
+            this.address1Label.Size = new System.Drawing.Size(121, 18);
+            this.address1Label.TabIndex = 7;
+            this.address1Label.Text = "Street Address 1:";
+            // 
+            // address2Label
+            // 
+            this.address2Label.AutoSize = true;
+            this.address2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.address2Label.Location = new System.Drawing.Point(649, 109);
+            this.address2Label.Name = "address2Label";
+            this.address2Label.Size = new System.Drawing.Size(121, 18);
+            this.address2Label.TabIndex = 8;
+            this.address2Label.Text = "Street Address 2:";
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cityLabel.Location = new System.Drawing.Point(649, 157);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(37, 18);
+            this.cityLabel.TabIndex = 9;
+            this.cityLabel.Text = "City:";
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stateLabel.Location = new System.Drawing.Point(984, 157);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(46, 18);
+            this.stateLabel.TabIndex = 10;
+            this.stateLabel.Text = "State:";
+            // 
+            // zipCodeLabel
+            // 
+            this.zipCodeLabel.AutoSize = true;
+            this.zipCodeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zipCodeLabel.Location = new System.Drawing.Point(649, 199);
+            this.zipCodeLabel.Name = "zipCodeLabel";
+            this.zipCodeLabel.Size = new System.Drawing.Size(72, 18);
+            this.zipCodeLabel.TabIndex = 11;
+            this.zipCodeLabel.Text = "Zip Code:";
+            // 
+            // phoneNumberLabel
+            // 
+            this.phoneNumberLabel.AutoSize = true;
+            this.phoneNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneNumberLabel.Location = new System.Drawing.Point(652, 244);
+            this.phoneNumberLabel.Name = "phoneNumberLabel";
+            this.phoneNumberLabel.Size = new System.Drawing.Size(112, 18);
+            this.phoneNumberLabel.TabIndex = 12;
+            this.phoneNumberLabel.Text = "Phone Number:";
+            // 
             // RegisterPatientUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.phoneNumberLabel);
+            this.Controls.Add(this.zipCodeLabel);
+            this.Controls.Add(this.stateLabel);
+            this.Controls.Add(this.cityLabel);
+            this.Controls.Add(this.address2Label);
+            this.Controls.Add(this.address1Label);
+            this.Controls.Add(this.ssnLabel);
+            this.Controls.Add(this.genderLabel);
+            this.Controls.Add(this.dateOfBirthLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.lastNameLabel);
             this.Controls.Add(this.clearButton);
             this.Controls.Add(this.registerButton);
             this.MaximumSize = new System.Drawing.Size(1215, 615);
@@ -64,6 +196,7 @@ namespace Group3_ClinicDB.UserControls
             this.Name = "RegisterPatientUserControl";
             this.Size = new System.Drawing.Size(1215, 615);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +204,16 @@ namespace Group3_ClinicDB.UserControls
 
         private System.Windows.Forms.Button registerButton;
         private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label dateOfBirthLabel;
+        private System.Windows.Forms.Label genderLabel;
+        private System.Windows.Forms.Label ssnLabel;
+        private System.Windows.Forms.Label address1Label;
+        private System.Windows.Forms.Label address2Label;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.Label zipCodeLabel;
+        private System.Windows.Forms.Label phoneNumberLabel;
     }
 }
