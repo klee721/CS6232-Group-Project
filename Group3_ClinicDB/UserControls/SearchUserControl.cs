a2edf369ebb3cd20452f315fee9b9cbfbd3da04a
Jordan Barron <jbarron8@my.westga.edu>
2021-03-28 19:08:53 -0400

    Beginning implementation of checked buttons

--- Group3_ClinicDB/UserControls/SearchUserControl.cs
+++ Group3_ClinicDB/UserControls/SearchUserControl.cs
@@ -13,21 +13,63 @@ namespace Group3_ClinicDB.UserControls
         private void InitSearches()
         {
             this.dobSearchRadioButton.Checked = true;
-
-            this.dobLastNameSearchRadioButton.Checked = false;
-            this.dobDoblnSearchDateTimePicker.Enabled = false;
-            this.lastNameDoblnSearchTextBox.ReadOnly = true;
-            this.doblnSearchButton.Enabled = false;
-
-            this.fnlnSearchRadioButton.Checked = false;
-            this.firstNameFnlnSearchTextBox.ReadOnly = true;
-            this.lastNameFnlnSearchTextBox.ReadOnly = true;
-            this.fnlnSearchButton.Enabled = false;
+            this.DisableFirstNameLastNameSearch(true);
+            this.DisableDobLastNameSearch(true);
         }
 
         private void SearchUserControlLoad(object sender, EventArgs e)
         {
             this.InitSearches();
+        }
+
+        private void DisableDobSearch(bool disable)
+        {
+            if (disable)
+            {
+                this.dobSearchRadioButton.Checked = false;
+                this.dobSearchDateTimePicker.Enabled = false;
+                this.dobSearchButton.Enabled = false;
+            }
+            else
+            {
+                this.dobSearchRadioButton.Checked = true;
+                this.dobSearchDateTimePicker.Enabled = true;
+                this.dobSearchButton.Enabled = true;
+            }
+        }
+
+        private void DisableFirstNameLastNameSearch(bool disable)
+        {
+            if (disable)
+            {
+                this.fnlnSearchRadioButton.Checked = false;
+                this.firstNameFnlnSearchTextBox.ReadOnly = true;
+                this.lastNameFnlnSearchTextBox.ReadOnly = true;
+                this.fnlnSearchButton.Enabled = false;
+            } else
+            {
+                this.fnlnSearchRadioButton.Checked = true;
+                this.firstNameFnlnSearchTextBox.ReadOnly = false;
+                this.lastNameFnlnSearchTextBox.ReadOnly = false;
+                this.fnlnSearchButton.Enabled = true;
+            }
+        }
+
+        private void DisableDobLastNameSearch(bool disable)
+        {
+            if (disable)
+            {
+                this.dobLastNameSearchRadioButton.Checked = false;
+                this.dobDoblnSearchDateTimePicker.Enabled = false;
+                this.lastNameDoblnSearchTextBox.ReadOnly = true;
+                this.doblnSearchButton.Enabled = false;
+            } else
+            {
+                this.dobLastNameSearchRadioButton.Checked = true;
+                this.dobDoblnSearchDateTimePicker.Enabled = false;
+                this.lastNameDoblnSearchTextBox.ReadOnly = false;
+                this.doblnSearchButton.Enabled = true;
+            }
         }
     }
 }
